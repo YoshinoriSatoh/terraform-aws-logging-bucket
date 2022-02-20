2d1becdf65a2cac5c183f5131326b36eba80b124
YoshinoriSatoh <yoshinori.satoh.tokyo@gmail.com>
2022-02-20 10:09:56 +0900

    up

--- main.tf
+++ main.tf
@@ -104,10 +104,10 @@ resource "aws_s3_bucket_acl" "cloudfront" {
       }
       permission = "FULL_CONTROL"
     }
-  }
 
-  owner {
-    id = "c4c1ede66af53448b93c283ce9448c4ba468c9432aa01d700d3878632f77d2d0"
+    owner {
+      id = "c4c1ede66af53448b93c283ce9448c4ba468c9432aa01d700d3878632f77d2d0"
+    }
   }
 }
 
